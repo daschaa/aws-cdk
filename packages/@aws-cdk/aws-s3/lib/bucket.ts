59d431a46f7c6e103a5662b87a6fde912b7ad45f
Joshua Weber <josh@joshuaw.de>
2022-05-19 21:15:25 +0200

    :sparkles: Adds ObjectSizeGreaterThan to lifecycle rules

--- packages/@aws-cdk/aws-s3/lib/bucket.ts
+++ packages/@aws-cdk/aws-s3/lib/bucket.ts
@@ -1920,6 +1920,7 @@ export class Bucket extends BucketBase {
         })),
         expiredObjectDeleteMarker: rule.expiredObjectDeleteMarker,
         tagFilters: self.parseTagFilters(rule.tagFilters),
+        objectSizeGreaterThan: rule.objectSizeGreaterThan,
       };
 
       return x;
